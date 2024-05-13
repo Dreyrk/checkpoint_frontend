49fa5765921cf1f86fd5ac60f326b10f35d2d025
Dreyrk <lucas.rondepierre123@gmail.com>
2024-05-13 12:49:38 +0200

    add header on every pages

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,15 +1,11 @@
-import Header from "@/components/Header";
 import CountryList from "@/components/countries/CountryList";
 import CreateForm from "@/components/countries/CreateForm";
 
 export default function Home() {
   return (
-    <>
-      <Header />
-      <main className="flex flex-col items-center gap-10 mt-8">
-        <CreateForm />
-        <CountryList />
-      </main>
-    </>
+    <main className="flex flex-col items-center gap-10 mt-8">
+      <CreateForm />
+      <CountryList />
+    </main>
   );
 }
